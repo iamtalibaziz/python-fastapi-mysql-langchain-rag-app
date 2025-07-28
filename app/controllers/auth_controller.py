50f47c57a86d87c2b31c2c37e7781d0a3d1dc628
Talib Aziz <iamtalibaziz@gmail>
2025-07-28 13:17:07 +0530

    Moved try except to global

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -4,7 +4,7 @@ from app.schemas import user_schema
 from app.services import auth_service
 from app.configs.database import get_db
 from datetime import datetime
-from app.helpers.response_helper import success_response, error_response
+from app.helpers.response_helper import success_response
 from app.utils.logger import logger
 from app.helpers.exceptions import CustomException
 
@@ -24,33 +24,21 @@ def signin(request: user_schema.SignInRequest, db: Session = Depends(get_db)):
     access_token = auth_service.create_access_token(
         data={"sub": user.email}
     )
-    return success_response(data={"access_token": access_token, "token_type": "bearer"})
+    return success_response(data={"access_token": access_token, "token_type": "Bearer"})
 
 @router.post("/forgot-password")
 def forgot_password(request: user_schema.ForgotPasswordRequest, db: Session = Depends(get_db)):
-    try:
-        user = auth_service.get_user_by_email(db, email=request.email)
-        if not user:
-            raise CustomException(message="User not found", status_code=status.HTTP_404_NOT_FOUND)
-        reset_token = auth_service.create_reset_token(db, user)
-        # In a real application, you would send this token to the user's email
-        return success_response(data={"reset_token": reset_token})
-    except CustomException as e:
-        return error_response(message=e.message, status_code=e.status_code, errors=e.errors)
-    except Exception as e:
-        logger.error(f"Error during forgot password: {e}")
-        raise e
+    user = auth_service.get_user_by_email(db, email=request.email)
+    if not user:
+        raise CustomException(message="User not found", status_code=status.HTTP_404_NOT_FOUND)
+    reset_token = auth_service.create_reset_token(db, user)
+    # In a real application, you would send this token to the user's email
+    return success_response(data={"reset_token": reset_token})
 
 @router.post("/reset-password")
 def reset_password(request: user_schema.ResetPasswordRequest, db: Session = Depends(get_db)):
-    try:
-        user = auth_service.get_user_by_reset_token(db, token=request.reset_token)
-        if not user or user.reset_token_expires < datetime.utcnow():
-            raise CustomException(message="Invalid or expired token", status_code=status.HTTP_400_BAD_REQUEST)
-        auth_service.reset_password(db, user, request.new_password)
-        return success_response(data={"message": "Password updated successfully"})
-    except CustomException as e:
-        return error_response(message=e.message, status_code=e.status_code, errors=e.errors)
-    except Exception as e:
-        logger.error(f"Error during reset password: {e}")
-        raise e
+    user = auth_service.get_user_by_reset_token(db, token=request.reset_token)
+    if not user or user.reset_token_expires < datetime.utcnow():
+        raise CustomException(message="Invalid or expired token", status_code=status.HTTP_400_BAD_REQUEST)
+    auth_service.reset_password(db, user, request.new_password)
+    return success_response(data={"message": "Password updated successfully"})
--- app/schemas/response_schema.py
+++ app/schemas/response_schema.py
@@ -1,12 +1,37 @@
-from pydantic import BaseModel, Field
-from typing import TypeVar, Generic, List, Any
+from pydantic import BaseModel
+from typing import TypeVar, Generic, List
 
 T = TypeVar('T')
 
-class ResponseModel(BaseModel, Generic[T]):
-    success: bool = Field(..., description="Indicates if the request was successful")
-    data: T = Field(..., description="The response data")
 
-class ErrorResponse(BaseModel):
-    success: bool = False
-    detail: str
+class Meta(BaseModel):
+    page: int
+    size: int
+    total_records: int
+    total_pages: int
+
+class PaginatedResponse(BaseModel, Generic[T]):
+    success: bool = True
+    data: List[T]
+    meta: Meta
+
+
+class ListResponse(BaseModel, Generic[T]):
+    success: bool
+    data: List[T]
+
+class ListWithTotalResponse(BaseModel, Generic[T]):
+    success: bool
+    data: List[T]
+    total_records: int
+
+class SingleResponse(BaseModel, Generic[T]):
+    success: bool = True
+    data: T
+
+class SuccessOnlyResponse(BaseModel):
+    success: bool = True
+
+class SuccessWithIdOnlyResponse(BaseModel):
+    success: bool = True
+    id: int
