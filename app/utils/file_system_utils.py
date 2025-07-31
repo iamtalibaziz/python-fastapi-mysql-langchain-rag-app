07d036f6534151cacb1d9271430be2d1c38165a5
Talib Aziz <iamtalibaziz@gmail>
2025-07-31 16:46:13 +0530

    Implemented RAG using document_url

--- app/utils/file_system_utils.py
+++ app/utils/file_system_utils.py
@@ -8,3 +8,10 @@ def save_uploaded_file(file: UploadFile, file_path: str):
     """
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
+
+def save_downloaded_file(content: bytes, file_path: str):
+    """
+    Saves the downloaded file to the specified path.
+    """
+    with open(file_path, "wb") as buffer:
+        buffer.write(content)
